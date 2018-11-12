6c04b3549f5892790fd7e46e5a53d13899873c56
alexfont <alexfont321@gmail.com>
2018-11-12 15:08:24 -0600

    saving chinook query

--- chinookQuery.sql
+++ chinookQuery.sql
@@ -2,11 +2,13 @@
 select 
 c.CustomerId,
 c.FirstName,
-c.LastName
+c.LastName,
+c.Country
 from Customer c
 where c.Country = "Brazil"
 ;
 */ 
+
 
 select 
 c.FirstName,
@@ -21,3 +23,21 @@ where c.Country = "Brazil"
 
 
 select * from Employee e where e.Title = "Sales Support Agent";
+
+
+select distinct
+i.BillingCountry
+from Invoice i 
+;
+
+select 
+i.InvoiceId,
+i.Total,
+i.BillingAddress,
+i.CustomerId,
+e.FirstName ||" "|| e.LastName as FullName
+from Invoice i
+join Customer c on c.CustomerId = i.CustomerId
+join Employee e on e.EmployeeId = c.SupportRepId
+order by FullName
+;
